e1b1b254be40bd4963cb07789259de06a63c2758
程应根 <chengyinggen@lukou.com>
2020-02-10 09:29:12 +0800

    新增LeetCode581

---
+++ leetcode/src/main/java/cn/jack/leetcode/MyMain.java
@@ -0,0 +1,4 @@
+package cn.jack.leetcode;
+
+public class MyMain {
+}
---
+++ leetcode/src/main/java/cn/jack/leetcode/leetcode581/Test.java
@@ -0,0 +1,46 @@
+package cn.jack.leetcode.leetcode581;
+
+/**
+ * 581. 最短无序连续子数组
+ * https://leetcode-cn.com/problems/shortest-unsorted-continuous-subarray/
+ */
+public class Test {
+
+    public static void main(String[] args) {
+//        int[] nums = {2, 6, 4, 8, 10, 9, 15};
+//        int[] nums = {2, 1};
+//        int[] nums = {1, 2, 3, 4, 5};
+//        int[] nums = {1, 3, 2, 2, 2};
+        int[] nums = {1, 3, 2, 2, 2, 4, 4, 5};
+        System.out.println(findUnsortedSubarray(nums));
+    }
+
+    public static int findUnsortedSubarray(int[] nums) {
+        if (null == nums || nums.length <= 1) {
+            return 0;
+        }
+        int len = nums.length;
+        int start = nums[0];
+        int end = nums[len - 1];
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < len; i++) {
+            if (nums[i] < start) {
+                right = i;
+            } else {
+                start = nums[i];
+            }
+        }
+        for (int i = len - 1; i >= 0; i--) {
+            if (nums[i] > end) {
+                left = i;
+            } else {
+                end = nums[i];
+            }
+        }
+        if (right - left == 0) {
+            return 0;
+        }
+        return right - left + 1;
+    }
+}
